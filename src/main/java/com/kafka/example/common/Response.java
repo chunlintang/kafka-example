a9cecf6acf4773f9bc8262218adbea0bcc96f303
Mantis <tangchunlinit@163.com>
2018-10-10 08:42:55 +0800

    kafka example

---
+++ src/main/java/com/kafka/example/common/Response.java
@@ -0,0 +1,17 @@
+package com.kafka.example.common;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+public class Response {
+    private int code;
+    private String message;
+
+    public Response(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+}
---
+++ src/main/java/com/kafka/example/controller/ProducerController.java
@@ -0,0 +1,17 @@
+package com.kafka.example.controller;
+
+import com.google.gson.Gson;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/kafka")
+public class ProducerController {
+
+}
