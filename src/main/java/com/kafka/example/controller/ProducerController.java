fd3ed08ee7ac0c0808ad1ab6f6552db5fe3a5a55
Mantis <tangchunlinit@163.com>
2018-10-10 23:33:08 +0800

    code

--- src/main/java/com/kafka/example/controller/ProducerController.java
+++ src/main/java/com/kafka/example/controller/ProducerController.java
@@ -1,6 +1,10 @@
 package com.kafka.example.controller;
 
 import com.google.gson.Gson;
+import com.kafka.example.common.ErrorCode;
+import com.kafka.example.common.MessageEntity;
+import com.kafka.example.common.Response;
+import com.kafka.example.producer.SimpleProducer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,5 +17,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/kafka")
 public class ProducerController {
+    @Autowired
+    private SimpleProducer simpleProducer;
 
+    @Value("${kafka.topic.default}")
+    private String topic;
+
+    private Gson gson = new Gson();
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET, produces = {"application/json"})
+    public Response sendKafka() {
+        return new Response(ErrorCode.SUCCESS, "OK");
+    }
+
+    @RequestMapping(value = "/send", method = RequestMethod.POST, produces = {"application/json"})
+    public Response sendKafka(@RequestBody MessageEntity messageEntity) {
+        try {
+            simpleProducer.send(topic, "key", messageEntity);
+            return new Response(ErrorCode.SUCCESS, "SEND SUCCESS");
+        } catch (Exception e) {
+            return new Response(ErrorCode.EXCEPTION, "SEND ERROR");
+        }
+    }
 }
